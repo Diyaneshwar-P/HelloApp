b4a8a2c9701c8ce0bc37ba582efd5ce7c668d8f5
Diyaneshwar-P <dp7925@srmist.edu.in>
2026-03-26 08:05:12 +0530

    uc6 done

--- src/main/HelloApp.java
+++ src/main/HelloApp.java
@@ -1,20 +1,19 @@
 public class HelloApp {
     public static void main(String[] args) {
         StringBuilder nameBuilder = new StringBuilder();
-        boolean first = true;
 
         for (String name : args) {
-            if (!first) {
-                nameBuilder.append(", ");
-            }
             nameBuilder.append(name);
-            first = false;
+            nameBuilder.append(", ");
         }
 
-        if (args.length == 0) {
-            nameBuilder.append("World");
+        String greeting;
+        if (nameBuilder.length() > 0) {
+            greeting = nameBuilder.substring(0, nameBuilder.length() - 2); // Remove the last ", "
+        } else {
+            greeting = "World";
         }
 
-        System.out.println("Hello, " + nameBuilder + "!");
+        System.out.println("Hello, " + greeting + "!");
     }
 }
